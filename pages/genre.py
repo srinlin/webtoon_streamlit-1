0254bb101b95d1262b6e2f0d2a9414e29c3c3b7b
chuchacha <107037722+chuchacha@users.noreply.github.com>
2022-08-17 22:46:06 +0900

    Update genre.py

--- pages/genre.py
+++ pages/genre.py
@@ -23,8 +23,6 @@ gd = GridOptionsBuilder.from_dataframe(df)
 gd.configure_selection(selection_mode='multiple', use_checkbox=True)
 gridoptions = gd.build()
 
-grid_table = AgGrid(df, height=250, gridOptions=gridoptions,
-                    update_mode=GridUpdateMode.SELECTION_CHANGED)
 st.header('웹툰의 제목을 입력해주세요 :)')
 options = st.multiselect(
      '웹툰 제목을 입력하고 Enter를 눌러주세요.',
@@ -33,6 +31,11 @@ select_area = st.empty()
 
 if not options:
     print(st.empty().info("입력을 기다리는 중~~"))
+    
+grid_table = AgGrid(df, height=250, gridOptions=gridoptions,
+                    update_mode=GridUpdateMode.SELECTION_CHANGED)
+
+
    
 st.write('## Selected')
 selected_row = grid_table["selected_rows"]
